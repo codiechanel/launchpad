0efe02133c551f5ce38868861605c08b9c2ef0c0
linuxadmin <codiechanel@gmail.com>
2017-08-11 13:30:33 +0800

    mesos

---
+++ scripts/installMesosUbuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "addng key -------------------------------------------------------"
+vsudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "add repo ----------------------------------------------"
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | 
+  sudo tee /etc/apt/sources.list.d/mesosphere.list
+echo "apt update -------------------------------------------"
+sudo apt-get -y update
+echo "install packages -----------------------------------------"
+sudo apt-get -y install mesos marathon chronos
+echo "contanerizers -----------------------------------------"
+cat <<EOF >> /etc/mesos-slave/containerizers
+docker,mesos
+EOF
+echo "starting services --------------------------------"
+sudo service zookeeper restart
+sudo service mesos-master restart
+sudo service mesos-slave restart
+sudo service marathon restart
